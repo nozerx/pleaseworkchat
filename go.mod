0ad47452fceb31afef96171544bf628d94d0c5a0
nozerx <104958959+nozerx@users.noreply.github.com>
2022-11-26 00:31:25 +0530

    first working copy
    verified by daison

--- go.mod
+++ go.mod
@@ -51,6 +51,7 @@ require (
 	github.com/libp2p/go-libp2p-asn-util v0.2.0 // indirect
 	github.com/libp2p/go-libp2p-core v0.20.0 // indirect
 	github.com/libp2p/go-libp2p-kbucket v0.4.7 // indirect
+	github.com/libp2p/go-libp2p-pubsub v0.8.1 // indirect
 	github.com/libp2p/go-libp2p-record v0.2.0 // indirect
 	github.com/libp2p/go-msgio v0.2.0 // indirect
 	github.com/libp2p/go-nat v0.1.0 // indirect
@@ -95,6 +96,7 @@ require (
 	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/whyrusleeping/go-keyspace v0.0.0-20160322163242-5b898ac5add1 // indirect
+	github.com/whyrusleeping/timecache v0.0.0-20160911033111-cfcb2f1abfee // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 )
 
+const service string = "chatapp/rezon"
 
-func createNode() {
+func createNode() (host.Host, context.Context) {
 	host, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
 	if err != nil {
 		fmt.Println("Error in establishing a node")
@@ -51,15 +58,121 @@ func createNode() {
 			}
 
 		}()
-		waitGroup.Wait()
-		routingDiscovery := drouting.NewRoutingDiscovery()
 
+	}
+	waitGroup.Wait()
+	fmt.Println(len(host.Network().Peers()))
+	return host, ctx
+}
+
+func main() {
+	host, ctx := createNode()
+	sub, topic := newPubSub(ctx, host, service)
+	go discoverPeers(ctx, host)
+	go getMessageFromSubscription(ctx, sub)
+	streamHandler(ctx, topic)
+}
+
+func initDHT(ctx context.Context, host host.Host) *dht.IpfsDHT {
+	kad_dht, err := dht.New(ctx, host)
+	if err != nil {
+		fmt.Println("Error while creating a new dht")
+	} else {
+		fmt.Println("Successfully created a new dht")
+	}
+	return kad_dht
+}
+
+func discoverPeers(ctx context.Context, host host.Host) {
+	kad_dht := initDHT(ctx, host)
+	routingDiscovery := drouting.NewRoutingDiscovery(kad_dht)
+	dutil.Advertise(ctx, routingDiscovery, "service1")
+	fmt.Println(len(host.Network().Peers()))
+
+	connectedList := []peer.AddrInfo{}
+	for len(connectedList) < 5 {
+		peerChan, err := routingDiscovery.FindPeers(ctx, "service1")
+		fmt.Println(len(host.Network().Peers()))
+
+		if err != nil {
+			fmt.Println("Error while finding peers after advertising")
+		} else {
+			fmt.Println("Successfully found peers after advertising")
+		}
+		fmt.Println("Connected to ", len(connectedList), "out of 5 nodes")
+		fmt.Println(connectedList)
+		loopSkip := false
+		for peerAddr := range peerChan {
+			if peerAddr.ID == host.ID() {
+				continue
+			}
+			for _, prAddr := range connectedList {
+				if prAddr.ID == peerAddr.ID {
+					loopSkip = true
+				}
+			}
+			if loopSkip {
+				continue
+			}
+			err := host.Connect(ctx, peerAddr)
+			if err != nil {
+				// fmt.Println("Error while connecting to the peer :", peerAddr.ID, peerAddr.Addrs)
+			} else {
+				fmt.Println("Successfully connected to the peer :", peerAddr.ID, peerAddr.Addrs)
+				connectedList = append(connectedList, peerAddr)
+			}
+		}
+	}
+	fmt.Println("Announcement: Connected to these peers")
+	fmt.Println(connectedList)
+
+}
+
+func newPubSub(ctx context.Context, host host.Host, service string) (*pubsub.Subscription, *pubsub.Topic) {
+	pubSubService, err := pubsub.NewGossipSub(ctx, host)
+	if err != nil {
+		fmt.Println("Error while establishing a pubsub service")
+	} else {
+		fmt.Println("Successfull in establishing a pubsub service")
+	}
+	topic, err := pubSubService.Join(service)
+	if err != nil {
+		fmt.Println("Error whie joining the", service, "service")
+	} else {
+		fmt.Println("Successfully joined the ", service, "service")
+	}
+	subscription, err := topic.Subscribe()
+	if err != nil {
+		fmt.Println("Error while subscribing to ", topic, "service")
+	} else {
+		fmt.Println("Successfully subscribed to ", topic, "service")
+	}
+	return subscription, topic
+}
+
+func getMessageFromSubscription(ctx context.Context, sub *pubsub.Subscription) {
+	for {
+		m, err := sub.Next(ctx)
+		if err != nil {
+			fmt.Println("Error while reading from subsciption", sub.Topic())
+		} else {
+			fmt.Println("Successfully read from subscription", sub.Topic())
+		}
+		fmt.Println(m.ReceivedFrom, " : ->", string(m.Message.Data))
 	}
 }
 
-func main(
-	createNode()
-)
-// func initDHT()
-// func advertise()
-// func discoverPeers()
+func streamHandler(ctx context.Context, topic *pubsub.Topic) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error while reading a string")
+		}
+		err = topic.Publish(ctx, []byte(s))
+		if err != nil {
+			fmt.Println("Error while publishing the message :>", s)
+		}
+
+	}
+}
